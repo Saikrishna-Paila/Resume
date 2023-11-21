43a35374e76a14ce0ce73b72684dc70ea23e8551
G39129775 <saikrishna.paila@gwmail.gwu.edu>
2023-11-21 14:54:56 -0500

    adding files

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('year').textContent = new Date().getFullYear();
+    
+    // Additional JavaScript for interactive elements will go here
+});
