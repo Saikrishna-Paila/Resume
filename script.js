34c264798ebaae6d01f7c785ffd61898c7d574ab
Saikrishna-Paila <saikrishna.paila@gwmail.gwu.edu>
2024-06-13 12:31:26 +0530

    Files ADDED

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('year').textContent = new Date().getFullYear();
-    
-    // Additional JavaScript for interactive elements will go here
-});
